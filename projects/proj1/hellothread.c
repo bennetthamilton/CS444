0b29f02487ead28f4a65dc6480bb2942a807deaf
bennetthamilton <bennetthamilton0@gmail.com>
2024-04-03 10:25:27 -0700

    added header

---
+++ projects/proj1/hellothread.c
@@ -0,0 +1,8 @@
+// Title: Project 1: Multithreading
+// Name: Bennett Hamilton
+// Collaborators: 
+// Date: 4/3/24
+// Description: a simple multithreaded program running in C. 
+//              The program will spawn two threads. Each one 
+//              will print out its "thread identifier" followed 
+//              by a colon, followed by the numbers 0 to 4.
