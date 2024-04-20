aa31603ed062313ab9564b7181ed18ff9341c301
bennetthamilton <bennetthamilton0@gmail.com>
2024-04-20 13:01:19 -0700

    added global semaphores, defined initial functions for producer and consumer, initialize buffer and threads and semaphores, join threads, clean up

--- projects/proj3/eventbuf/pc.c
+++ projects/proj3/eventbuf/pc.c
@@ -12,6 +12,10 @@
 #include <semaphore.h>
 #include "eventbuf.c"
 
+// Global variables
+struct eventbuf *eb;
+sem_t *empty, *full, *mutex;
+
 sem_t *sem_open_temp(const char *name, int value)
 {
     sem_t *sem;
@@ -29,6 +33,20 @@ sem_t *sem_open_temp(const char *name, int value)
     return sem;
 }
 
+void *producer(void *arg)
+{
+    // Produce events
+
+    return NULL;
+}
+
+void *consumer(void *arg)
+{
+    // Consume events
+
+    return NULL;
+}
+
 int main(int argc, char *argv[]) 
 {
 
@@ -38,19 +56,56 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    // Define arguments
+    int producer_count = atoi(argv[1]);
+    int consumer_count = atoi(argv[2]);
+    int event_count = atoi(argv[3]);
+    int max_event = atoi(argv[4]);
+
     // Initialize the buffer
+    eb = eventbuf_create();
+
+    // Initialize semaphores
+    empty = sem_open_temp("/empty", event_count);
+    full = sem_open_temp("/full", 0);
+    mutex = sem_open_temp("/mutex", 1);
 
     // Start producer threads
+    pthread_t producer_threads[producer_count];
+
+    for (int i = 0; i < producer_count; i++) {
+        // Create a new thread
+        pthread_create(&producer_threads[i], NULL, producer, (void *)&i);
+    }
 
     // Start consumer threads
+    pthread_t consumer_threads[consumer_count];
+
+    for (int i = 0; i < consumer_count; i++) {
+        // Create a new thread
+        pthread_create(&consumer_threads[i], NULL, consumer, (void *)&i);
+    }
 
     // Wait for all producer threads to finish
+    for (int i = 0; i < producer_count; i++) {
+        pthread_join(producer_threads[i], NULL);
+    }
 
     // Notify all consumer threads that there will be no more events
+    for (int i = 0; i < consumer_count; i++) {
+        sem_post(full);
+    }
 
     // Wait for all consumer threads to finish
+    for (int i = 0; i < consumer_count; i++) {
+        pthread_join(consumer_threads[i], NULL);
+    }
 
-    // Free the buffer
+    // Clean up
+    eventbuf_free(eb);
+    sem_close(empty);
+    sem_close(full);
+    sem_close(mutex);
 
     return 0;
 }
