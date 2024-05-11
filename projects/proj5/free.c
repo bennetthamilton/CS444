e84585c951fea722308f9f80ddaf36ddb4276670
bennetthamilton <bennetthamilton0@gmail.com>
2024-05-11 13:44:59 -0700

    implement set_free

--- projects/proj5/free.c
+++ projects/proj5/free.c
@@ -1,8 +1,9 @@
 #include "free.h"
 
 void set_free(unsigned char *block, int num, int set) {
-    int byte = num / 8;
-    int bit = num % 8;
+    int byte_i = num / 8;
+    int bit_i = num % 8;
+    block[byte_i] = (block[byte_i] & ~(1 << bit_i)) | (set << bit_i);
 }
 
 int find_free(unsigned char *block) {
