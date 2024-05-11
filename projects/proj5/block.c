7541e2ba0d1a8cb4945e56dffce871f6966cf902
bennetthamilton <bennetthamilton0@gmail.com>
2024-05-11 14:25:13 -0700

    implement alloc

--- projects/proj5/block.c
+++ projects/proj5/block.c
@@ -1,7 +1,7 @@
 #include "image.h"
 #include "block.h"
+#include "free.h"
 #include <unistd.h>
-
 
 unsigned char *bread(int block_num, unsigned char *block){
     // Find the block offset using block number and size
@@ -23,4 +23,15 @@ void bwrite(int block_num, unsigned char *block){
     // Write the block to the image file
     // ref: https://man7.org/linux/man-pages/man2/write.2.html
     write(image_fd, block, BLOCK_SIZE);
+}
+
+int alloc(void){
+    unsigned char block[BLOCK_SIZE];
+    bread(FREE_BLOCK, block);
+    int free_block = find_free(block);
+    if (free_block != -1) {
+        set_free(block, free_block, 1);
+        bwrite(FREE_BLOCK, block);
+    }
+    return free_block;
 }
