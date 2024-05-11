df687ce42313f52b81b47cac17ac3943922b4263
bennetthamilton <bennetthamilton0@gmail.com>
2024-05-11 13:42:07 -0700

    init commit

---
+++ projects/proj5/free.c
@@ -0,0 +1,10 @@
+#include "free.h"
+
+void set_free(unsigned char *block, int num, int set) {
+    int byte = num / 8;
+    int bit = num % 8;
+}
+
+int find_free(unsigned char *block) {
+    return 0;
+}
